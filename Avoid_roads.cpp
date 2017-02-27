206dedf34e30361d6cbb41c253ce9f0af850f63d
shubhamag97 <shubhamag97@gmail.com>
2017-02-27 23:57:34 +0530

    Create Avoid_roads.cpp

---
+++ Avoid_roads.cpp
@@ -0,0 +1,17 @@
+// we basically follow the dp; dp[i][j]=0, if bad node;
+//                             dp[i][j]=dp[i-1][j]+dp[i][j-1], else;
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--)
+	{
+	int w,d;string s;cin>>w>>d;
+	getline(cin,s);
+	getline(cin,s);
+	int dp[w][d];
+	while(
+	}
+	return 0;
+}
